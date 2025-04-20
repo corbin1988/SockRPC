505cb02cb35e892e9c5aba476d4176f2f8e504bc
KyleHurst <kyle.c.hurst@gmail.com>
2025-04-19 21:56:28 -0300

    Setup commit

---
+++ src/SockRPC.Core/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SockRPC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("SockRPC is alive!");
+        }
+    }
+}
